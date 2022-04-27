59f2027df68edab7ccb53c1505131f39125c1d00
Utkarsh Bhatt <utkarsh_bhatt@outlook.com>
2022-04-27 17:31:48 +0530

    Moved Test file to the asset directory.

--- example_Readfromfile.cpp
+++ example_Readfromfile.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include "parser.h"
 
+std::string assetPath = "./assets";
+
 
 int main() {
     
-    std::fstream file("test.txt");
+    std::fstream file(assetPath + "/test.txt");
     parser parserObj(file);
     std::cout << parserObj.word_stat() << "\n";
 }
