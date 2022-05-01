049d87119f7e7b7ca3a7ecac5a245abc98ac2dc2
Utkarsh Bhatt <utkarsh_bhatt@outlook.com>
2022-05-01 11:17:00 +0530

    Completed documentation.

--- docs/QITs/QIT1: Summary Improvement.md
+++ docs/QITs/QIT1: Summary Improvement.md
@@ -12,8 +12,16 @@
  ### Effort Analysis
  | Description | LoCs | Author |
  | ----------- | ---- | ------ |
- | Addition of another vector to contain the sorted list of word->count pair, Mergesort implementation of batch wise list merge. | 200 | Utkarsh Bhatt |
+ | Addition of another vector to contain the sorted list of word->count pair, related sorted CRUD code | 150 | Utkarsh Bhatt |
  | Word Stat pretty printing | 50 | Utkarsh Bhatt |
 
  ### Code Description
+ Files Added:
+    1. containerHelper.hpp: Contains Helper functions for different std containers for miscellaneous tasks.
+
+ Overall Implementation Description:
+ A new data member is added to the Parser Class, this sorted vector is how we fetch the most and least frequent words of the wordmap. Since the parser design is reetrant, i.e. the parser can be called repetetively on succesive blobs of txt and update the wordmap, the vector also needs to be updated everytime a new batch is passed, for this, a batch wise design is implemented and std::merge is used to merge two already sorted vectors (class member sorted vector and batch sorted vector).
+
  ### Author Notes
+ One catch with the sorting process is that since the map is a lexically ordered container, The most and least frquent words are sorted on 2 basis, lexicality and count, where count takes priority ofcourse. It means, if ther are 2 words "Apple" and "Hello" both with frequency 15 competing for the 5th most frequent word, "Apple" would be chosen since it comes prior lexically (dictionary sort).
+ Author: Utkarsh Bhatt (utkarsh_bhatt@outlook.com)
--- lib/include/containerHelper.hpp
+++ lib/include/containerHelper.hpp
@@ -8,6 +8,13 @@
 
 namespace wordmap {
 
+    /**
+     * @brief Add a word to the map of word->count pairs, if already present increase count.
+     * 
+     * @param base_map
+     * @param word
+     * @return uint64_t Frequency of the newly added word.
+     */
     uint64_t addToMap(std::map<std::string, uint64_t> &base_map, std::string word)
     {
         uint64_t curr_count = 0;
@@ -23,13 +30,19 @@ namespace wordmap {
         return curr_count;
     }
 
+    /**
+     * @brief Convert a Map with word->count pairs to a vector of such pairs, sorted on the basis of count.
+     * 
+     * @param map 
+     * @param vec 
+     */
     void mapToSortedArray(std::map<std::string, uint64_t> &map, std::vector<std::pair<std::string, uint64_t>> &vec)
     {
         vec.reserve(map.size());	// Expand vector to be as long as map.
         std::transform(map.begin(), map.end(), std::back_inserter(vec), [](auto &pair) {return pair;});
         std::sort(vec.begin(), vec.end(), [](std::pair<std::string, uint64_t> &a, std::pair<std::string, uint64_t> &b)
             {
-                return (a.second > b.second);   // Comparator for Descending Sort of word->count pairs.
+                return (a.second >= b.second);   // Comparator for Descending Sort of word->count pairs.
             });
         return;
     }
@@ -37,6 +50,12 @@ namespace wordmap {
 
 namespace sorted_vec {
 
+    /**
+     * @brief Merge a sorted vector to another inplace, i.e. once done, final_vec will be a sorted union of itself and to_merge_vec.
+     * 
+     * @param final_vec 
+     * @param to_merge_vec 
+     */
     void mergeInVector(std::vector<std::pair<std::string, uint64_t>> &final_vec, std::vector<std::pair<std::string, uint64_t>> &to_merge_vec)
     {
         std::vector<std::pair<std::string, uint64_t>> temp_vec(final_vec.size());
@@ -50,6 +69,12 @@ namespace sorted_vec {
 
 namespace stat_stream {
 
+    /**
+     * @brief Populate the Stream with the stats of Most/Least frequent words.
+     * 
+     * @param stream 
+     * @param sorted_vec 
+     */
     void fillStatFromSortedVector(std::stringstream &stream, std::vector<std::pair<std::string, uint64_t>> &sorted_vec)
     {
         if (sorted_vec.empty()) {
